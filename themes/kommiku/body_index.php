51426a41966090677642e859d2ae3c36be53b7a8
anraiki <anraiki@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-08 00:05:11 +0000

    -Missing files. Well kind of. Attempting to fix a old error.
    git-svn-id: https://plugins.svn.wordpress.org/kommiku/trunk@226248 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ themes/kommiku/body_index.php
@@ -0,0 +1,42 @@
+<?php kommiku_header(); ?>
+
+<div id="content" class="narrowcolumn home">
+    <div class="column">
+    	<h2>Series Listing</h2>
+		<ul>
+        	<?php echo $kommiku['series_list']; ?>
+    	</ul>
+	</div>
+	<div class="column">
+		<h2>Chapter Updates</h2>
+		<ul>
+		<?php if (is_array($chapterUpdates)) { ?>
+			<?php foreach ($chapterUpdates as $item) {?>
+				<li>
+					<a href="<?php echo HTTP_HOST.KOMMIKU_URL_FORMAT.'/'.$item->series_slug.'/'.$item->chapter_slug.'/'; ?>"><?php echo $item->series_name." - Chapter ".$item->chapter_slug; ?></a>
+					<span style="float: right;"><?php echo strftime('%D',strtotime($item->pubdate)); ?></span>
+				</li>
+			<?php } }?>
+		</ul>
+	</div>
+	<div class="column">
+		<h2>Page Updates</h2>
+			<ul>
+			<?php if (is_array($pageUpdates)) { ?>
+				<?php foreach ($pageUpdates as $item) {?>
+					<?php if ($item->chapterless == '1') { ?>
+					<li>
+						<a href="<?php echo HTTP_HOST.KOMMIKU_URL_FORMAT.'/'.$item->series_slug.'/'.$item->page_slug.'/'; ?>"><?php echo $item->series_name." - Page ".$item->page_slug; ?></a>
+						<span style="float: right;"><?php echo strftime('%D',strtotime($item->pubdate)); ?></span>
+					</li>
+				<?php } else if (isset($item->chapter_slug)) { ?>
+					<li>
+						<a href="<?php echo HTTP_HOST.KOMMIKU_URL_FORMAT.'/'.$item->series_slug.'/'.$item->chapter_slug.'/'.$item->page_slug.'/'; ?>"><?php echo $item->series_name." - Ch ".$item->chapter_slug." - Page ".$item->page_slug; ?></a>
+						<span style="float: right;"><?php echo strftime('%D',strtotime($item->pubdate)); ?></span>
+					</li>
+				<?php } } }?>
+			</ul>
+	</div>
+</div>
+
+<?php kommiku_footer(); ?>
